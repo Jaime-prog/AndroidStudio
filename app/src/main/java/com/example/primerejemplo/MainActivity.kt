2b2d2d29d95dbe9bd7314dc7fcb374df1abf399d
Jaime-prog <A00571842@tec.mx>
2022-08-16 19:53:01 -0500

    Initial commit

---
+++ app/src/main/java/com/example/primerejemplo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.primerejemplo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var myTxt : String
+
+        var myTextView =findViewById<TextView>(R.id.textView2)
+        var myEditText = findViewById<EditText>(R.id.editTxt)
+        var myButton = findViewById<Button>(R.id.button2)
+
+        myButton.setOnClickListener {
+            myTxt= myEditText.text.toString();
+            myTextView.text=myTxt;
+        }
+    }
+}
